2933f1745e89a175c07b41e9f6161a0ae38b0bed
alecs <lecs.dumitru@gmail.com>
2016-10-30 13:19:41 +0000

    first commit - angular generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name billSplitApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the billSplitApp
+ */
+angular.module('billSplitApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name billSplitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the billSplitApp
+ */
+angular.module('billSplitApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

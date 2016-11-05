7f6dc8e6164e32e78d099aab6dd07ae02fb79f1d
alecs <lecs.dumitru@gmail.com>
2016-11-05 10:42:54 +0000

    deployin@

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,53 +8,58 @@
  * Controller of the billSplitApp
  */
 angular.module('billSplitApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $localStorage) {
+
         $scope.dial_btns = [1,2,3,4,5,6,7,8,9,"",0, "<-"];
 
+        $scope.$localStorage = $localStorage;
         $scope.total = "0.00";
+        $scope.payer = "";
+        $scope.showChoosePayer = false;
+        $scope.message = "";
 
         $scope.friends = [];
 
         $scope.init = function(){
             $scope.friends.push({
                 name: "Andreo", surname: "C.", active: false, avatar: "", amount: 0.00, fix: false
-            })
+            });
 
             $scope.friends.push({
                 name: "Dragos", surname: "C.", active: false, avatar: "", amount: 0.00, fix: false
-            })
+            });
 
             $scope.friends.push({
                 name: "Marius", surname: "P.", active: false, avatar: "", amount: 0.00, fix: false
-            })
+            });
 
             $scope.friends.push({
                 name: "Geo", surname: "P.", active: false, avatar: "", amount: 0.00, fix: false
-            })
-        }()
+            });
+        }();
 
 
         $scope.update_split_amount = function(){
-            var friendsEqual = $scope.friends.filter(x => x.active && !x.fix);
+            var friendsEqual = $scope.friends.filter(function(x){ return x.active && !x.fix});
 
-            var friendsFix = $scope.friends.filter(x => x.active && x.fix);
+            var friendsFix = $scope.friends.filter(function(x){return x.active && x.fix});
             var totalFix = friendsFix.
-            reduce((a,b)=> { 
+            reduce(function(a,b){ 
                 return Number(a) + parseFloat(Number(b.amount).toFixed(2)); 
             }, 0);
 
             var totalNoFix = $scope.total - totalFix;
 
-            for(let friend of friendsEqual){
-                friend.amount = (Number(totalNoFix) / parseFloat(friendsEqual.length)).toFixed(2);
+            for(var friend in friendsEqual){
+                friendsEqual[friend].amount = (Number(totalNoFix) / parseFloat(friendsEqual.length)).toFixed(2);
 
-                console.log(friend.amount);
+                console.log(friendsEqual[friend].amount);
             }
-        }
+        };
 
         $scope.$watch('friends',function(o,n){
             $scope.update_split_amount();
-        }, true)
+        }, true);
 
 
         $scope.append_number = function(val){
@@ -68,5 +73,46 @@ angular.module('billSplitApp')
             $scope.total = (x * 10).toFixed(2).toString();
 
             $scope.update_split_amount();
-        }
+        };
+
+        $scope.beforeSubmitSplit = function(){
+            var transaction = getNewTransaction();
+
+            if( transaction.total === "0.00" ){
+                return;
+            }
+
+            $scope.showChoosePayer = true;
+        };  
+
+        $scope.submitSplit = function(){
+            var transaction = getNewTransaction();
+            var transactionsRef = database.ref('transactions');
+            transactionsRef.push().set(angular.copy(transaction));
+
+            transactionsRef.on('child_added', function(data) {
+              $scope.message = data;
+              setTimeout(function(){
+
+                 $scope.$apply(function(){ $scope.showChoosePayer = false; });
+
+                 setTimeout(function(){
+                    location.reload();
+                 },300)
+              }, 1000);
+            });
+        };
+
+        var getNewTransaction = function() {
+            var transaction = {
+                date : new Date().toString(),
+                details : "",
+                payment : $scope.friends.filter(function(x){ return x.active}),
+                payedBy: "",
+                status : "pending",
+                total: $scope.total
+            };
+
+            return transaction;
+        };
   });
